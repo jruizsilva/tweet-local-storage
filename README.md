f7e5240f26af5702879a1cd5fa8a4cea2155d321
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2021-05-19 19:01:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Buscador Autos
+
+## JavaScript Moderno Project #4
+
+### Overview
+
+![Preview](https://i.ibb.co/6mVCKxN/Tweet-Local-Storage.png)
+
+- Aprendi a guardar datos en el local storage
+- Aprendi a utilizar template y fragment para evitar el reflow cuando insertamos contenido HTML mediante JS
+
+[Udemy Course](https://www.udemy.com/course/javascript-moderno-guia-definitiva-construye-10-proyectos/)
+
+### Contact
+
+- Website [jruizsilva.netlify.app](https://jruizsilva.netlify.app)
+- GitHub [@jruizsilva](https://github.com/jruizsilva)
+- Twitter [@jnt077](https://twitter.com/jnt077)
---
+++ js/app.js
@@ -0,0 +1,80 @@
+// Variables
+const formulario = document.querySelector("#form");
+const inputTweet = formulario.querySelector("#tweet");
+const listaTweetHTML = document.querySelector("#lista-tweets");
+const btnSubmit = formulario.querySelector("#submit");
+let arrayTweets = [];
+// Functions
+const iniciarApp = () => {
+	resetFormulario();
+	disableSubmit();
+	getData();
+	cargarEventListener();
+};
+const resetFormulario = () => {
+	formulario.reset();
+};
+const disableSubmit = () => {
+	btnSubmit.disabled = true;
+	btnSubmit.classList.remove("cursor-allowed");
+	btnSubmit.classList.add("opacity-50", "cursor-not-allowed");
+};
+const enableSubmit = () => {
+	btnSubmit.disabled = false;
+	btnSubmit.classList.remove("opacity-50", "cursor-not-allowed");
+	btnSubmit.classList.add("cursor-allowed");
+};
+const validarTweet = (e) => {
+	if (e.target.value === "") {
+		disableSubmit();
+	} else {
+		enableSubmit();
+	}
+};
+const agregarTweet = (e) => {
+	e.preventDefault();
+	const tweet = {
+		content: inputTweet.value,
+		id: Date.now(),
+	};
+	arrayTweets.push(tweet);
+	localStorage.setItem("tweets", JSON.stringify(arrayTweets));
+	mostrarTweets();
+};
+const mostrarTweets = () => {
+	limpiarHTML();
+	const template = document.querySelector("#template-li").content;
+	const fragment = document.createDocumentFragment();
+	arrayTweets.forEach((tweet) => {
+		const { content, id } = tweet;
+		template.querySelector("#borrar-item").setAttribute("data-id", id);
+		template.querySelector("p.text-tweet").textContent = content;
+		const clone = template.cloneNode(true);
+		fragment.appendChild(clone);
+	});
+	listaTweetHTML.appendChild(fragment);
+};
+const limpiarHTML = () => {
+	while (listaTweetHTML.firstChild) {
+		listaTweetHTML.removeChild(listaTweetHTML.firstChild);
+	}
+};
+const borrarTweet = (e) => {
+	if (e.target.classList.contains("borrar-item")) {
+		const id = parseInt(e.target.getAttribute("data-id"));
+		arrayTweets = arrayTweets.filter((tweet) => tweet.id !== id);
+		mostrarTweets();
+		localStorage.setItem("tweets", JSON.stringify(arrayTweets));
+	}
+};
+const getData = () => {
+	arrayTweets = JSON.parse(localStorage.getItem("tweets"));
+	mostrarTweets();
+};
+// Events
+const cargarEventListener = () => {
+	inputTweet.addEventListener("blur", validarTweet);
+	formulario.addEventListener("submit", agregarTweet);
+	listaTweetHTML.addEventListener("click", borrarTweet);
+};
+document.addEventListener("DOMContentLoaded", iniciarApp);
